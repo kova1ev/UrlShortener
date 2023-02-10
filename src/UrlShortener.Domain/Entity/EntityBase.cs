971dae5043c8c9b72c1c5913f9698c8d91542b42
kovalev <kov92av@gmail.com>
2023-02-10 22:38:08 +0300

    add base entity

---
+++ src/UrlShortener.Domain/Entity/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UrlShortener.Domain.Entity;
+
+public abstract class EntityBase
+{
+    [Required]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    public DateTimeOffset DateTimeCreated { get; set; } = DateTime.Now;
+}

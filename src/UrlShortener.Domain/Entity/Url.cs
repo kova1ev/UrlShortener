27c462e3a9c89cbf78b7cd08d87363e0d48b4584
kovalev <kov92av@gmail.com>
2023-02-11 18:29:24 +0300

    add url entity

---
+++ src/UrlShortener.Domain/Entity/Url.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.Domain.Entity;
+
+public class Url : EntityBase
+{
+    public Url(string urlAddress, string urlShort) : base()
+    {
+        UrlAddress = urlAddress ?? throw new ArgumentNullException(nameof(urlAddress));
+        UrlShort = urlShort ?? throw new ArgumentNullException(nameof(urlShort));
+    }
+
+    [Required]
+    public string UrlAddress { get; set; }
+    [Required]
+    public string UrlShort { get; set; }
+
+
+}

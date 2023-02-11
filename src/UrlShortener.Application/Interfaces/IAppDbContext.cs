2c186c2ec329975c3309e2a92668be37ad2186b2
kovalev <kov92av@gmail.com>
2023-02-11 18:02:08 +0300

    add interface for appdbcontext

---
+++ src/UrlShortener.Application/Interfaces/IAppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Domain.Entity;
+
+namespace UrlShortener.Application.Interfaces;
+public interface IAppDbContext
+{
+    DbSet<Url> Urls { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellation);
+}

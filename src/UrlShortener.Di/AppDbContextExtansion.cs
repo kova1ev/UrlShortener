48cb7d9d3cf71928113c8cd942021f7a7dddc85a
kovalev <kov92av@gmail.com>
2023-02-11 18:28:59 +0300

    add di project +  appdbcontext server collection extan..

---
+++ src/UrlShortener.Di/AppDbContextExtansion.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UrlShortener.Application.Interfaces;
+using UrlShortener.Data;
+
+namespace UrlShortener.Di;
+
+public static class AppDbContextExtansion
+{
+
+    public static void AddAppDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IAppDbContext>(provider => provider.GetRequiredService<AppDbContext>());
+
+        string connectionString = configuration["ConnectionStrings:MsSql"]
+            ?? throw new ArgumentNullException($"{nameof(configuration)} does not contain connectionStrings");
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+    }
+}

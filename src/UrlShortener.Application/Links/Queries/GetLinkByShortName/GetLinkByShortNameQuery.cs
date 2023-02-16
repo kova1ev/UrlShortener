759b3e63769f0b31162734f4745000b56b43fdc9
kovalev <kov92av@gmail.com>
2023-02-17 00:25:18 +0300

    add get kink by short name query + handler

---
+++ src/UrlShortener.Application/Links/Queries/GetLinkByShortName/GetLinkByShortNameQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using UrlShortener.Domain.Entity;
+
+namespace UrlShortener.Application.Links.Queries.GetLinkByShortName;
+
+public class GetLinkByShortNameQuery : IRequest<Link>
+{
+    public string ShortName { get; set; }
+
+    public GetLinkByShortNameQuery(string shortName)
+    {
+        ShortName = shortName;
+    }
+}
---
+++ src/UrlShortener.Application/Links/Queries/GetLinkByShortName/GetLinkByShortNameQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Application.Interfaces;
+using UrlShortener.Domain.Entity;
+
+namespace UrlShortener.Application.Links.Queries.GetLinkByShortName;
+
+public class GetLinkByShortNameQueryHandler : IRequestHandler<GetLinkByShortNameQuery, Link?>
+{
+    private readonly IAppDbContext _appDbContext;
+
+    public GetLinkByShortNameQueryHandler(IAppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext ?? throw new ArgumentNullException(nameof(appDbContext));
+    }
+
+    public async Task<Link?> Handle(GetLinkByShortNameQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _appDbContext.Links.FirstOrDefaultAsync(l => l.ShortName == request.ShortName);
+
+        return result;
+    }
+}
+

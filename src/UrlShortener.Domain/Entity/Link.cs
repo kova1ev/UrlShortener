53d6877b5131f0712ed6a937a3add06dc4fb57ec
kovalev <kov92av@gmail.com>
2023-02-21 22:21:58 +0300

    update domain entity

--- src/UrlShortener.Domain/Entity/Link.cs
+++ src/UrlShortener.Domain/Entity/Link.cs
@@ -6,21 +6,14 @@ namespace UrlShortener.Domain.Entity;
 [Table("links")]
 public class Link : EntityBase
 {
-    //public Link(s) : base()
-    //{
-    //    ShortName = shortName ?? throw new ArgumentNullException(nameof(shortName));
-    //    UrlAddress = urlAddress ?? throw new ArgumentNullException(nameof(urlAddress));
-    //}
-
     [Required]
     public string UrlAddress { get; set; }
     [Required]
-    public string ShortName { get; set; }
+    public string Alias { get; set; }
     public DateTime DateTimeCreated { get; private set; } = DateTime.UtcNow;
 
     [Required]
     public LinkInfo LinkInfo { get; set; }
-
 }
 
 
--- src/UrlShortener.Domain/Entity/LinkInfo.cs
+++ src/UrlShortener.Domain/Entity/LinkInfo.cs
@@ -5,24 +5,17 @@ namespace UrlShortener.Domain.Entity;
 [Table("linkinfos")]
 public class LinkInfo : EntityBase
 {
-    //public LinkInfo(DateTime lastUse, Link Link)
-    //{
-    //    LastUse = lastUse;
-    //    this.Link = Link;
-    //    LinkId = Link.Id;
-    //    LastUse = lastUse;
-    //    DomainName = GetDomainName(Link.UrlAddress); //?? throw new ArgumentNullException(nameof(urlAddress));
-    //}
-
     public Guid LinkId { get; set; }
     public Link Link { get; set; }
     public string DomainName { get; set; }
     public DateTime LastUse { get; set; }
 
 
-
+    //TODO   CHECK URL? THROW EX?
     public string GetDomainName(string url)
     {
+        if (url == null)
+            throw new ArgumentNullException(nameof(url));
         var array = url.Split('/');
         if (array.Length < 3)
             throw new Exception($"invalid url : {url}");

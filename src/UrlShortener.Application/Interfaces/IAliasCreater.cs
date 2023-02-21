4ca3c70faa2c4e7522b41f2aa584aacd7fa56556
kovalev <kov92av@gmail.com>
2023-02-21 23:06:04 +0300

    add IAliasCteater + simply implement

---
+++ src/UrlShortener.Application/Interfaces/IAliasCreater.cs
@@ -0,0 +1,6 @@
+﻿namespace UrlShortener.Application.Interfaces;
+
+public interface IAliasCteater
+{
+    string CreateAlias(string url);
+}
---
+++ src/UrlShortener.Application/Services/AliasCreater.cs
@@ -0,0 +1,15 @@
+﻿using UrlShortener.Application.Interfaces;
+
+namespace UrlShortener.Application.Services;
+
+internal class AliasCreater : IAliasCteater
+{
+    //TODO Implement creater!!
+    public string CreateAlias(string url)
+    {
+        // check generated shortName in DB
+        // generate again ? 
+        int hash = url.GetHashCode();
+        return hash.ToString().TrimStart('-');
+    }
+}

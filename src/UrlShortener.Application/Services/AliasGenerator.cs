faf0c7570bc8c28c77209ec8187c7caacb0a399d
kovalev <kov92av@gmail.com>
2023-03-25 15:55:57 +0300

    fix GenerateAlias

--- src/UrlShortener.Application/Services/AliasGenerator.cs
+++ src/UrlShortener.Application/Services/AliasGenerator.cs
@@ -5,20 +5,21 @@ namespace UrlShortener.Application.Services;
 
 internal class AliasGenerator : IAliasGenerator
 {
+    private const string SOURCE = "abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVWXYZ";
     public string GenerateAlias(int minLength = 4, int maxLength = 10)
     {
+        if (minLength < 3) throw new ArgumentException($"{nameof(minLength)} must be above 3.");
+        if (maxLength <= minLength) throw new ArgumentException($"{nameof(maxLength)} must be above {nameof(minLength)}.");
+
         Random rnd = new Random();
-        string source = "abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVWXYZ";
         int length = rnd.Next(minLength, maxLength + 1);
-
         StringBuilder sb = new(length);
-
         for (int i = 0; i < length; i++)
         {
-            int index = rnd.Next(0, source.Length);
-            sb.Append(source[index]);
+            int index = rnd.Next(0, SOURCE.Length);
+            sb.Append(SOURCE[index]);
         }
-        return sb.ToString();
 
+        return sb.ToString();
     }
 }

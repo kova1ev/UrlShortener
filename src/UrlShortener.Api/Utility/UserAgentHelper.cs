850c3f924a267beeeadecc9c92399fde8de8de0d
kovalev <kov92av@gmail.com>
2023-03-30 15:28:12 +0300

    # add UserAgentHelper

---
+++ src/UrlShortener.Api/Utility/UserAgentHelper.cs
@@ -0,0 +1,24 @@
+﻿using DeviceDetectorNET;
+using UrlShortener.Application.Common.Models;
+
+namespace UrlShortener.Api.Utility;
+
+public class UserAgentHelper
+{
+    public UserAgentInfo Parse(string userAgent)
+    {
+        UserAgentInfo userAgentInfo = new();
+        DeviceDetector deviceDetector = new DeviceDetector(userAgent);
+
+        deviceDetector.Parse();
+        if (!deviceDetector.IsBot())
+        {
+            userAgentInfo.Browser = deviceDetector.GetClient().Match.Name;
+            userAgentInfo.Os = deviceDetector.GetOs().Match?.Name;
+            userAgentInfo.Device = deviceDetector.GetDeviceName();
+            userAgentInfo.Brand = deviceDetector.GetBrandName();
+            userAgentInfo.Model = deviceDetector.GetModel();
+        }
+        return userAgentInfo;
+    }
+}
